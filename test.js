eb6e0e2c6efc8deab06ad94079dd73376a19da50
David Banks <amoebae@gmail.com>
2016-07-02 13:52:19 +0100

    red test that a surrounded true will flip

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var assert = require('chai').assert;
 
 var something = require('./main.js');
 
-it('next step is correct', function () {
+it('uniform grid stays uniform', function () {
     var grid = [
         [false, false, false],
         [false, false,  false],
@@ -21,6 +21,25 @@ it('next step is correct', function () {
     var nextGrid = something.nextStep(grid);
     assert.deepEqual(expectedGrid, nextGrid);
 });
+
+it('surrounded true will flip', function () {
+    var grid = [
+        [false, false, false],
+        [false, true,  false],
+        [false, false, false]
+    ];
+
+    var expectedGrid = [
+        [false, false, false],
+        [false, false, false],
+        [false, false, false]
+    ];
+
+
+    var nextGrid = something.nextStep(grid);
+    assert.deepEqual(expectedGrid, nextGrid);
+});
+
 
 it('should be correct', function () {
     assert.equal(42, something.meaningOfLife());
