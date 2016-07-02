ea3c63c131b6a293004a8ca8c6f8ea94d0ff071d
David Banks <amoebae@gmail.com>
2016-07-02 13:18:02 +0100

    update expected data and add lookup table for vote

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 // The identity CA
+
+// This is effectively used as a dictionary to determine the next state.
+// [Rucker & Walker, 1997]
+var nextState = [
+    false, false, false, false, false,
+    true, true, true, true, true
+]
+
 function nextStep(grid) {
     return grid;
 }
