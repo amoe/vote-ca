393218a3e9c5033c3fd0640afeda7be0cfed1107
David Banks <amoebae@gmail.com>
2016-07-02 15:08:12 +0100

    rendering a grid of squares

--- VoteAutomaton.vue
+++ VoteAutomaton.vue
@@ -1,9 +1,28 @@
 <template>
   <div>
-  <p>Hello, world!</p>
+    <p>Y size is {{ ySize }}, X size is {{ xSize }}</p>
+  
+  <table>
+
+    <tr v-for="n in ySize">
+      <td v-for="n in xSize">
+        &#x25a0; &#9633;
+      </td>
+    </tr>
+  </table>
+  
   </div>
 </template>
 
 <script>
 console.log("inside app component");
+
+export default {
+    data: function () {
+        return {
+            ySize: 64,
+            xSize: 64
+        }
+    }
+}
 </script>
