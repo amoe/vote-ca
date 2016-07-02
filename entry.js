cec26cf799fba983676424bf511f424439127aef
David Banks <amoebae@gmail.com>
2016-07-02 13:50:57 +0100

    implement lodash, iterating over grid elements

--- entry.js
+++ entry.js
@@ -2,5 +2,5 @@
 
 var mainModule = require('./main.js');
 
-var element = document.getElementById('main');
-element.textContent = "Hello, world!";
+//var element = document.getElementById('main');
+//element.textContent = "Hello, world!";
--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-// The identity CA
+import _ from 'lodash';
 
 // This is effectively used as a dictionary to determine the next state.
 // [Rucker & Walker, 1997]
-var nextState = [
+var nextStateMap = [
     false, false, false, false, false,
     true, true, true, true, true
 ]
@@ -11,12 +11,23 @@ function nextStep(grid) {
     var yRange = grid.length;
     var xRange = grid[0].length;    // FIXME: assert that grid is square
 
-    // ES6 only
-    // for (let xIndex of [1, 2, 3, 4, 5]) {
-    //     console.log("What?");
-    // }
+    var newGrid = _.cloneDeep(grid);
 
-    return grid;
+    // ES6 only
+    for (let yIndex in _.range(yRange)) {
+        for (let xIndex in _.range(xRange)) {
+            newGrid[yIndex][xIndex] = nextState(grid, yIndex, xIndex);
+        }
+    }
+
+    return newGrid;
+}
+
+function nextState(grid, yIndex, xIndex) {
+    // FIXME: assert arguments
+
+    // Identity CA
+    return grid[yIndex][xIndex];
 }
 
 
