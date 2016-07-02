09e110e6254977cbf6bf97d0c19ca165cc6fff19
David Banks <amoebae@gmail.com>
2016-07-02 15:34:56 +0100

    animate CA, allow user to randomize

--- VoteAutomaton.vue
+++ VoteAutomaton.vue
@@ -3,6 +3,8 @@
     <p>Y size is {{ ySize }}, X size is {{ xSize }}</p>
   
     <button v-on:click="timeStep">Step</button>
+    <button v-on:click="togglePlaying">Play/Pause</button>
+    <button v-on:click="randomize">Randomize</button>
 
     <table>
       <tr v-for="y in grid.length">
@@ -27,15 +29,31 @@ export default {
         return {
             ySize: 25,
             xSize: 50,
-            grid: []
+            grid: [],
+            isPlaying: false            
         }
     },
     created: function () {
-        this.grid = vote.randomGrid(this.ySize, this.xSize);
+        this.randomize();
+        setInterval(this.maybeStep, 2000);
     },
     methods: {
+        'randomize': function () {
+            this.grid = vote.randomGrid(this.ySize, this.xSize);
+        },
         'timeStep': function () {
             console.log("a step was requested");
+            this.grid = vote.nextStep(this.grid);
+        },
+        'togglePlaying': function () {
+            this.isPlaying = !this.isPlaying;
+        },
+        'maybeStep': function () {
+            console.log("step function called from interval");
+
+            if (this.isPlaying) {
+                this.timeStep();
+            }
         }
     }
 }
