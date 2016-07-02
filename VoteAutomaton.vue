a1833f1b28af6f3e5992a5a3ed0cec4379ea9b7e
David Banks <amoebae@gmail.com>
2016-07-02 15:27:44 +0100

    displaying the cell value

--- VoteAutomaton.vue
+++ VoteAutomaton.vue
@@ -2,14 +2,17 @@
   <div>
     <p>Y size is {{ ySize }}, X size is {{ xSize }}</p>
   
-  <table>
+    <button v-on:click="timeStep">Step</button>
 
-    <tr v-for="n in grid.length">
-      <td v-for="n in grid[n].length">
-        &#x25a0;
-      </td>
-    </tr>
-  </table>
+    <table>
+      <tr v-for="y in grid.length">
+        <td v-for="x in grid[y].length">
+
+          <span v-if="grid[y][x]">&#x25a0;</span>
+          <span v-else>&#9633;</span>
+        </td>
+      </tr>
+    </table>
   
   </div>
 </template>
@@ -22,13 +25,18 @@ console.log("inside app component");
 export default {
     data: function () {
         return {
-            ySize: 64,
-            xSize: 64,
+            ySize: 25,
+            xSize: 50,
             grid: []
         }
     },
     created: function () {
         this.grid = vote.randomGrid(this.ySize, this.xSize);
+    },
+    methods: {
+        'timeStep': function () {
+            console.log("a step was requested");
+        }
     }
 }
 </script>
